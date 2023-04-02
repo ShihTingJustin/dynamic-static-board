b4e19ac27d45c8d354e5d896bb957878d04de103
mb722656355s <justinhuang777@gmail.com>
2023-04-02 06:51:12 +0000

    test: add test case

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -8,7 +8,19 @@ const html = fs.readFileSync(path.resolve(__dirname, "../index.html"), "utf8");
 let dom;
 let container;
 
-describe("測試", () => {
+describe("原始 HTML", () => {
+  beforeEach(async () => {
+    dom = new JSDOM(html, { runScripts: "dangerously" });
+    container = dom.window.document.body;
+  });
+
+  it("#data-panel 沒有資料", () => {
+    const dataPanel = container.querySelector("#data-panel");
+    expect(dataPanel.children.length).toBe(0);
+  });
+});
+
+describe("測試動態資料", () => {
   beforeEach(async () => {
     dom = new JSDOM(html, { runScripts: "dangerously" });
     container = dom.window.document.body;
@@ -25,20 +37,75 @@ describe("測試", () => {
     });
   });
 
-  it("fa fa-thumbs-up 存在於 .ans element", () => {
-    const playerRecords = container.querySelectorAll(".table tbody tr");
+  it("測試 球員", () => {
+    const nameAns = ["櫻木花道", "流川楓", "赤木剛憲", "宮城良田", "三井壽"];
+    container.querySelectorAll("#data-panel tr").forEach((node, i) => {
+      expect(node.children[0].textContent).toBe(nameAns[i]);
+    });
+  });
 
-    expect(playerRecords[1].children[0].innerHTML.includes("流川楓")).toBe(
-      true
-    );
-    expect(
-      playerRecords[1].children[0].innerHTML.includes("fa fa-thumbs-up")
-    ).toBe(true);
-    expect(playerRecords[4].children[0].innerHTML.includes("三井壽")).toBe(
-      true
-    );
-    expect(
-      playerRecords[4].children[0].innerHTML.includes("fa fa-thumbs-up")
-    ).toBe(true);
+  it("測試 得分", () => {
+    const pointAns = ["0", "30", "16", "6", "21"];
+    container.querySelectorAll("#data-panel tr").forEach((node, i) => {
+      expect(node.children[1].children[0].textContent).toBe(pointAns[i]);
+      expect(node.children[1].children[1].className).toBe(
+        "fa fa-plus-circle up"
+      );
+      expect(node.children[1].children[2].className).toBe(
+        "fa fa-minus-circle down"
+      );
+    });
+  });
+
+  it("測試 籃板", () => {
+    const reboundAns = ["0", "6", "10", "0", "4"];
+    container.querySelectorAll("#data-panel tr").forEach((node, i) => {
+      expect(node.children[2].children[0].textContent).toBe(reboundAns[i]);
+      expect(node.children[2].children[1].className).toBe(
+        "fa fa-plus-circle up"
+      );
+      expect(node.children[2].children[2].className).toBe(
+        "fa fa-minus-circle down"
+      );
+    });
+  });
+
+  it("測試 助攻", () => {
+    const assistAns = ["0", "3", "0", "7", "3"];
+    container.querySelectorAll("#data-panel tr").forEach((node, i) => {
+      expect(node.children[3].children[0].textContent).toBe(assistAns[i]);
+      expect(node.children[3].children[1].className).toBe(
+        "fa fa-plus-circle up"
+      );
+      expect(node.children[3].children[2].className).toBe(
+        "fa fa-minus-circle down"
+      );
+    });
+  });
+
+  it("測試 抄截", () => {
+    const stealAns = ["0", "3", "0", "6", "0"];
+    container.querySelectorAll("#data-panel tr").forEach((node, i) => {
+      expect(node.children[4].children[0].textContent).toBe(stealAns[i]);
+      expect(node.children[4].children[1].className).toBe(
+        "fa fa-plus-circle up"
+      );
+      expect(node.children[4].children[2].className).toBe(
+        "fa fa-minus-circle down"
+      );
+    });
+  });
+
+  it("測試 阻攻", () => {
+    const blockAns = ["2", "0", "5", "0", "0"];
+    container.querySelectorAll("#data-panel tr").forEach((node, i) => {
+      expect(node.children[5].children[0].textContent).toBe(blockAns[i]);
+      expect(node.children[5].children[1].className).toBe(
+        "fa fa-plus-circle up"
+      );
+      expect(node.children[5].children[2].className).toBe(
+        "fa fa-minus-circle down"
+      );
+    });
   });
 });
